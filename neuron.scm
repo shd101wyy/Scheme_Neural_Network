4973562e9de962ffecfb65a1c2ffea42408fa0e2
Yiyi Wang <shd101wyy@sina.com>
2014-02-16 19:31:24 -0600

    improve code

--- neuron.scm
+++ neuron.scm
@@ -34,14 +34,19 @@
            'neuron]
           [(eq? msg 'activate) ;; activate current neuron
            (lambda []
-             (letrec [(helper (lambda [neurons-that-connected-to-self result]
+             (letrec [(helper (lambda [neurons-that-connected-to-self result weight]
                                 (if (null? neurons-that-connected-to-self) ;; finish
                                     (transfer-function (+ result bias))
                                     (let [(r (((car neurons-that-connected-to-self) 'activate)))]
                                       (helper (cdr neurons-that-connected-to-self)
-                                              (+ result r))))))]
-               (helper neurons-that-connected-to-self 0))
+                                              (+ result 
+                                                 (* (car weight) r))
+                                              (cdr weight))))))]
+               (helper neurons-that-connected-to-self 0 weight))
              )]
+          [(eq? msg 'set-transfer-function)
+           (lambda [fn]
+             (set! transfer-function fn))]
           [else (error "make-neuron Invalid Command")]))
   )
 ;; make input neurons
@@ -49,13 +54,24 @@
   (define input 0)
   (define type 'input-neuron)
   (define transfer-function neuron/purelin)
+  
+  (define weight 1)
+  (define bias 0)
+  
   (lambda [msg]
     (cond [(eq? msg 'set-input) ;; set input data
            (lambda [n] (set! input n))]
           [(eq? msg 'type) ;; get neuron type
            'input-neuron]
           [(eq? msg' activate) ;; activate input neuron
-           (lambda [] (transfer-function input))])
+           (lambda [] (transfer-function (+ (* input weight) 
+                                            bias)))]
+          [(eq? msg 'set-transfer-function)
+           (lambda [fn]
+             (set! transfer-function fn))]
+          [(eq? msg 'add-input-neuron)
+           (error "Input Neurons couldn't add input neurons")]
+          [else (error "make-neuron Invalid Command")])
     )
   )
 
@@ -101,6 +117,17 @@
                (set-input-data (cdr input-data) 
                                (+ i 1)))))
   
+  (define (activate-layer layer-#num) ;; activate no. layer-#num layer eg ((n 'activate-layer) 1) activate layer 1
+      (let* [(output-layer (vector-ref network layer-#num))
+             (output-layer-length (vector-length output-layer))]
+        (letrec [(helper (lambda [i]
+                           (if (= i output-layer-length)
+                               '()
+                               (let [(r (((vector-ref output-layer i) 'activate)))]
+                                 (cons r 
+                                       (helper (+ i 1)))))))]
+          (helper 0))
+        ))
   (lambda [msg]
     (cond [(eq? msg 'connect) ;; connect two neurons. eg ((n 'connect) '(0 0) '(1 0)) means connect neuron 0 at layer 0 to neuron 0 at layer 1
            (lambda [v0 v1]
@@ -118,23 +145,44 @@
                     (p (cadr v))
                     (neuron (vector-ref (vector-ref network l) p))] ;; activate that neuron and get activation result
                ((neuron 'activate))))]
-          [(eq? msg 'set-input-data)
+          [(eq? msg 'activate) ;; activate all and get outputs from output layers
+           (lambda []
+             (let* [(output-layer (vector-ref network (- layer-num 1)))
+                   (output-layer-length (vector-length output-layer))]
+               (letrec [(helper (lambda [i]
+                               (if (= i output-layer-length)
+                                   '()
+                                   (let [(r (((vector-ref output-layer i) 'activate)))]
+                                     (cons r 
+                                           (helper (+ i 1)))))))]
+                 (helper 0)))
+             )
+           ]
+          [(eq? msg 'activate-layer) ;; activate one layer
+           activate-layer]
+          [(eq? msg 'set-input-data) ;; set input data to input neurons 
            (lambda [input-data]
              (set-input-data input-data 0))]
+          [(eq? msg 'get-neuron)     ;; get neuron at position. eg ((n 'get-neuron) '(1 0)) get neuron at layer 1 index 0
+           (lambda [v]
+             (let [(l (car v))
+                   (p (cadr v))]
+               (vector-ref (vector-ref network l) p)))]
           ))
   )
 
 
 ;; test network
-(define x (make-network 2 1)) ;; 2 input neurons and 1 output neurons
+(define x (make-network 2 2)) ;; 2 input neurons and 1 output neurons
 (display x)
-((x 'connect) '(0 0) '(1 0))
-((x 'connect) '(0 1) '(1 0))
+((x 'connect) '(0 0) '(1 0))  ;; connect (0 0) and (1 0)
+((x 'connect) '(0 1) '(1 0))  ;; connect (0 1) and (1 0)
+((x 'connect) '(0 0) '(1 1))  ;; connect (0 0) and (1 1)
 
-((x 'set-input-data) '(3 4))
-((x 'activate-neuron) '(1 0))
-
-
+((x 'set-input-data) '(3 4)) ;; set input data
+((x 'activate-neuron) '(1 0)) ;; activate neuron at layer 1 index 0
+((x 'activate-layer) 0) ;; activate layer 0
+((x 'activate)) ;; activate output layer
 
 
 
